288d61cb2e6036a385fc847f0cf40abe63547d62
vaishaliicreative <vaishali@icreativetechnolabs.com>
2023-05-09 18:55:14 +0530

    2023-05-09 create module file for override login component. create snippet file

---
+++ custom/plugins/ICTECHBackendLoginByOTP/src/Resources/app/administration/src/module/sw-login/page/index/index.js
@@ -0,0 +1,48 @@
+import template from './sw-login.html.twig';
+
+const { Component } = Shopware;
+
+Component.override('sw-login',{
+    template,
+    props: {
+        hash: {
+            type: String,
+            default: null,
+        },
+    },
+
+    data() {
+        return {
+            isLoading: false,
+            isLoginSuccess: false,
+            isLoginError: false,
+        };
+    },
+    metaInfo() {
+        return {
+            title: this.title,
+        };
+    },
+    computed: {
+        title() {
+            const modulName = this.$tc('sw-login.general.mainMenuItemIndex');
+            const adminName = this.$tc('global.sw-admin-menu.textShopwareAdmin');
+
+            return `${modulName} | ${adminName}`;
+        },
+    },
+
+    methods: {
+        setLoading(val) {
+            this.isLoading = val;
+        },
+
+        loginError() {
+            this.isLoginError = !this.isLoginError;
+        },
+
+        loginSuccess() {
+            this.isLoginSuccess = !this.isLoginSuccess;
+        },
+    },
+})
